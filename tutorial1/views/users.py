cbd497b7239673ab5ca5057a9cb51dabc7c56ab7
Muntasir Sauvik <muntasir@sas030.cpdist.ca>
2020-10-21 16:38:43 -0600

    user delete function working

--- tutorial1/views/users.py
+++ tutorial1/views/users.py
@@ -1,13 +1,7 @@
-from pyramid.compat import escape
-import re
-from docutils.core import publish_parts
-
+from sqlalchemy.orm.exc import NoResultFound
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
-from pyramid.security import (
-    remember,
-    forget,
-    )
+
 from .. import models
 
 
@@ -66,9 +60,6 @@ def user_add(request):
 
 @view_config(route_name='user_modify', renderer='../templates/users/userModify.jinja2')
 def user_modify(request):
-    # if len(role.strip()) == 0 and len(username.strip()) == 0 and len(password1) == 0:
-    #     errors.append('Nothing to update')
-    # if len(username.strip()) == 0:
     message = ''
     username = ''
     role = ''
@@ -112,4 +103,44 @@ def user_modify(request):
 
 @view_config(route_name='user_remove', renderer='../templates/users/userRemove.jinja2')
 def user_remove(request):
-    return {}
+    message = ''
+    username = ''
+    role = ''
+    errors = []
+
+    if 'form.submitted' in request.params:
+        username = request.params['username']
+        password1 = request.params['password1']
+
+        # Validation
+        if len(username.strip()) == 0 and len(password1) == 0:
+            errors.append('Nothing entered')
+        elif len(password1) != 0 and not request.user.check_password(password1):
+            errors.append('You entered a wrong Password')
+        else:
+            try:
+                user = request.dbsession.query(models.User.name).filter_by(name=username).one()
+                print(user)
+                user1 = request.dbsession.query(models.User).filter_by(name=username).one()
+                request.dbsession.delete(user1)
+            except NoResultFound:
+                errors.append('Username does not exist')
+
+        messages = '\n'.join(errors)
+
+        # # Update Information
+        # if not len(errors):
+        #     # Update role
+        #     if len(role.strip()) != 0:
+        #         res.role = role
+        #     if len(username.strip()) != 0:
+        #         res.name = username
+        #     if len(password1) != 0 and password1 == password2:
+        #         res.set_password(password1)
+
+    return dict(
+        message=message,
+        errors=errors,
+        url=request.route_url('user_remove'),
+        username=username,
+    )
