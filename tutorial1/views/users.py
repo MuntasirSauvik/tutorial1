676aac646122c417be0498dd9da8f86954f8de49
Muntasir Sauvik <muntasir@sas030.cpdist.ca>
2020-10-21 11:07:11 -0600

    fixed routes for the user options

--- tutorial1/views/users.py
+++ tutorial1/views/users.py
@@ -1,9 +1,31 @@
-# from pyramid.view import view_config
-# from ..models import User
-#
-#
-# @view_config(route_name='get_users', renderer='../templates/wiki/view.jinja2')
-# def get_users(request):
-#     page = request.context.page
-#     res = request.dbsession.query(User).all()
-#     return res
+from pyramid.compat import escape
+import re
+from docutils.core import publish_parts
+
+from pyramid.httpexceptions import HTTPFound
+from pyramid.view import view_config
+from pyramid.security import (
+    remember,
+    forget,
+    )
+from .. import models
+
+
+@view_config(route_name='user_list', renderer='../templates/users/userList.jinja2')
+def user_list(request):
+    return {}
+
+
+@view_config(route_name='user_add', renderer='../templates/users/userAdd.jinja2')
+def user_add(request):
+    return {}
+
+
+@view_config(route_name='user_modify', renderer='../templates/users/userModify.jinja2')
+def user_modify(request):
+    return {}
+
+
+@view_config(route_name='user_remove', renderer='../templates/users/userRemove.jinja2')
+def user_remove(request):
+    return {}
