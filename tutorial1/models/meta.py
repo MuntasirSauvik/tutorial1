59c5f43f1f0ac47b6769a834a8a5a99cd726b785
Muntasir Sauvik <muntasir.sauvik@cpdist.ca>
2020-10-22 12:18:47 -0600

    Fix issue creating database with alembic

--- tutorial1/models/meta.py
+++ tutorial1/models/meta.py
@@ -1,13 +1,21 @@
+import uuid
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import MetaData
+
+
+def custom_constraint_name(constraint, table):
+    return 'sa_constraint_{}'.format(str(uuid.uuid4())[:6])
+
 
 # Recommended naming convention used by Alembic, as various different database
 # providers will autogenerate vastly different names making migrations more
 # difficult. See: http://alembic.zzzcomputing.com/en/latest/naming.html
 NAMING_CONVENTION = {
+    'custom_constraint_name': custom_constraint_name,
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "ck": "ck_%(table_name)s_%(custom_constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
